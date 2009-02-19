1553c5715a23bc296396b1b060c794a819301b6d
spearwolf <wolfger@spearwolf.de>
2009-02-19 17:25:53 +0100

    minor tweak

--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,4 @@
 # microdata_fu rails hooks
 ActionView::Base.send :include, MicrodataFuHelper
 ActionController::Base.send :include, MicrodataFu
-
 RAILS_DEFAULT_LOGGER.debug "** microdata_fu loaded"
