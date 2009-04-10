fa2d620af9e6ad1f10504e767858da7d0f4a376f
spearwolf <wolfger@spearwolf.de>
2009-04-10 15:57:08 +0200

    added microdata storage options (currently flash and session supported)

--- lib/microdata_fu.rb
+++ lib/microdata_fu.rb
@@ -1,7 +1,31 @@
 # MicrodataFu
 module MicrodataFu
 
-  def microdata key, value
-    (flash[:microdata] ||= {})[key.to_s] = value.respond_to?(:to_json) ? value.to_json : value.to_s.to_json
+  def microdata key, value, options = {}
+    value = value.respond_to?(:to_json) ? value.to_json : value.to_s.to_json
+    #if use_flash?(options)
+      #(flash[:microdata] ||= {})[key.to_s] = value
+    #elsif use_session?(options)
+    if use_session?(options)
+      (session[:microdata] ||= {})[key.to_s] = value
+    else
+      (flash[:microdata] ||= {})[key.to_s] = value
+    end
+    # TODO store[key.to_s] = value
   end
+
+  private
+
+    def use_flash? options
+      options[:flash] == true
+    end
+
+    def use_session? options
+      options[:session] == true
+    end
+
+    #def store
+      # TODO store
+      #@@md_store ||= {}
+    #end
 end
--- lib/microdata_fu_helper.rb
+++ lib/microdata_fu_helper.rb
@@ -1,7 +1,16 @@
 module MicrodataFuHelper
 
   def microdata_fu
-    md = flash[:microdata].map {|k,v| "'#{escape_javascript(k)}':#{v}" }.join(",")
-    javascript_tag "var microdata_fu = /* created by spearwolf <wolfger@spearwolf.de> */(function(){var _md_fu={#{md}};var md={get:function(k){return _md_fu[k];}};return{read:function(k,fn){var v=md.get(k);if(fn!=undefined&&v!=undefined){v=fn(v);}return v;}}})();"
+    md = microdata_values.map {|k,v| "'#{escape_javascript(k)}':#{v}" }.join(",")
+    javascript_tag "var microdata_fu = /* http://github.com/spearwolf/microdata_fu/tree/master */(function(){var _md_fu={#{md}};var md={get:function(k){return _md_fu[k];}};return{read:function(k,fn){var v=md.get(k);if(fn!=undefined&&v!=undefined){v=fn(v);}return v;}}})();"
   end
+
+  private
+
+    def microdata_values
+      h = {}
+      h.merge!(session[:microdata]) unless session[:microdata].nil?
+      h.merge!(flash[:microdata]) unless flash[:microdata].nil?
+      # TODO store
+    end
 end
